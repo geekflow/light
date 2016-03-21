23c4ac64a1d525bb5f1d1f2135d3c114f474dc3b
geeksaga <geeksaga@geeksaga.com>
2016-03-21 19:28:20 +0900

    add class redefine method

--- agent/src/main/java/com/geeksaga/light/agent/JavaAgent.java
+++ agent/src/main/java/com/geeksaga/light/agent/JavaAgent.java
@@ -15,8 +15,12 @@
  */
 package com.geeksaga.light.agent;
 
+import java.lang.instrument.ClassDefinition;
 import java.lang.instrument.Instrumentation;
+import java.util.List;
+import java.util.concurrent.CopyOnWriteArrayList;
 import java.util.concurrent.atomic.AtomicBoolean;
+import java.util.logging.Level;
 import java.util.logging.Logger;
 
 /**
@@ -60,6 +64,99 @@ public class JavaAgent {
             logger.warning("Light Agent already initialized. Skip agent loading.");
             return true;
         }
+    }
+
+    public static long sizeOf(Object o) {
+        assert getInstrumentation() != null;
+        return getInstrumentation().getObjectSize(o);
+    }
+
+    public static Instrumentation getInstrumentation() {
+        return instrumentation;
+    }
+
+    public static Class<?> findClass(String className) {
+        return findClass(getAllLoadedClasses(), className);
+    }
+
+    public static Class<?> findClass(String className, final ClassLoader classLoader) {
+        return findClass(getClassLoaderInitiatedClasses(classLoader), className);
+    }
+
+    public static Class<?> findClass(Class<?>[] classes, String className) {
+        if (classes == null || className == null || className.length() == 0) {
+            return null;
+        }
+
+        for (Class<?> aClass : classes) {
+            if (className.equals(aClass.getName())) {
+                return aClass;
+            }
+        }
+
+        return null;
+    }
+
+    public static List<Class<?>> findClassList(String className) {
+        return findClassList(getAllLoadedClasses(), className);
+    }
+
+    public static List<Class<?>> findClassList(Class<?>[] classes, String className) {
+        List<Class<?>> list = new CopyOnWriteArrayList<Class<?>>();
+
+        if (classes != null && className != null && className.length() > 0) {
+            for (Class<?> aClass : classes) {
+                if (className.equals(aClass.getName())) {
+                    list.add(aClass);
+                }
+            }
+        }
+
+        return list;
+    }
+
+    public static Class<?>[] getAllLoadedClasses() {
+        if (getInstrumentation() != null) {
+            return getInstrumentation().getAllLoadedClasses();
+        }
+
+        return new Class[0];
+    }
+
+    public static Class<?>[] getSystemClassLoaderInitiatedClasses() {
+        return getClassLoaderInitiatedClasses(ClassLoader.getSystemClassLoader());
+    }
+
+    public static Class<?>[] getClassLoaderInitiatedClasses(final ClassLoader classLoader) {
+        return getInstrumentation().getInitiatedClasses(classLoader);
+    }
+
+    public static boolean redefineClasses(ClassDefinition[] classDefinitions) {
+        try {
+            if (isRedefineClassesSupported()) {
+                getInstrumentation().redefineClasses(classDefinitions);
+
+                return true;
+            }
+        } catch (Throwable throwable) {
+            if (classDefinitions != null) {
+                for (ClassDefinition classDefinition : classDefinitions) {
+                    logger.info(classDefinition.getDefinitionClass().toString());
+                }
+            }
+
+            logger.log(Level.INFO, throwable.getMessage(), throwable);
+        }
+
+        return false;
+    }
+
+    public static boolean redefineClasses(Class<?> clazz, byte[] buff) {
+        return isRedefineClassesSupported() && redefineClasses(new ClassDefinition[]{new ClassDefinition(clazz, buff)});
+    }
+
+    public static boolean isRedefineClassesSupported() {
+        return getInstrumentation() != null && getInstrumentation().isRedefineClassesSupported();
     }
 
     private static void failInitialize() {
