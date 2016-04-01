b3f01005a238a194947ffc4da28b9885602f230c
geeksaga <geeksaga@geeksaga.com>
2016-04-01 16:01:18 +0900

    update some system property value

--- common/src/main/java/com/geeksaga/light/util/SystemProperty.java
+++ common/src/main/java/com/geeksaga/light/util/SystemProperty.java
@@ -19,7 +19,40 @@ package com.geeksaga.light.util;
  * @author geeksaga
  */
 public class SystemProperty {
+    public static final boolean IS_JAVA_16 = getSystemProperty("java.version", "1.6").startsWith("1.6");
+    public static final boolean IS_JAVA_17 = getSystemProperty("java.version", "1.7").startsWith("1.7");
+    public static final boolean IS_JAVA_18 = getSystemProperty("java.version", "1.8").startsWith("1.8");
+    public static final boolean IS_JAVA_19 = getSystemProperty("java.version", "1.9").startsWith("1.9");
+    public static final boolean IS_JAVA_16_PLUS = IS_JAVA_16 || IS_JAVA_17 || IS_JAVA_18;
+    public static final boolean IS_JAVA_17_PLUS = IS_JAVA_17 || IS_JAVA_18;
+    public static final boolean IS_JAVA_18_PLUS = IS_JAVA_18 || IS_JAVA_19;
+    public static final boolean IS_JAVA_19_PLUS = IS_JAVA_19;
+
+    public static final String JAVA_CLASS_PATH = getSystemProperty("java.class.path");
+    public static final String JAVA_VERSION = getSystemProperty("java.version");
+    public static final String JAVA_HOME = getSystemProperty("java.home");
+    public static final String JAVA_VM_NAME = getSystemProperty("java.vm.name");
+    public static final String JAVA_VM_VERSION = getSystemProperty("java.vm.version");
+    public static final String JAVA_VENDOR = getSystemProperty("java.vendor");
+    public static final String JAVA_VM_VENDOR = getSystemProperty("java.vm.vendor");
+    public static final String JVM = getSystemProperty("jvm");
+    public static final String OS_ARCH = getSystemProperty("os.arch");
+    public static final String OS_NAME = getSystemProperty("os.name");
+    public static final String OS_VERSION = getSystemProperty("os.version");
+    public static final String USER_HOME = getSystemProperty("user.home");
+    public static final String USER_TIMEZONE = getSystemProperty("user.timezone");
+    public static final String FILE_ENCODING = getSystemProperty("file.encoding");
+
     public static final boolean WINDOWS_OS = getSystemProperty("os.name", "unix").contains("Window");
+    public static final boolean IBM_VM = JAVA_VENDOR.toUpperCase().startsWith("IBM");
+
+    public static final String LINE_SEPARATOR = getSystemProperty("line.separator");
+    public static final String PATH_SEPARATOR = getSystemProperty("path.separator");
+
+    private static String getSystemProperty(String key)
+    {
+        return getSystemProperty(key, "");
+    }
 
     private static String getSystemProperty(String key, String def) {
         try {
