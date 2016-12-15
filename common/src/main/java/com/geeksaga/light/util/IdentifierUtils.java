c899ac5743b61474131b69485041f4178a121562
geeksaga <geeksaga@geeksaga.com>
2016-12-15 16:07:40 +0900

    moved repository to common module.

--- common/src/main/java/com/geeksaga/light/util/IdentifierUtils.java
+++ common/src/main/java/com/geeksaga/light/util/IdentifierUtils.java
@@ -13,7 +13,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package com.geeksaga.light.repository.util;
+package com.geeksaga.light.util;
 
 import java.util.UUID;
 
